aeb3ac85669f65d3f35bb3b9b959c9cdb9bb5e20
Fermin Ricolfe Loren <f.ricolfe@olrsoft.com>
2020-07-17 16:07:33 +0200

    Update Program.cs

--- TestSFTP/Program.cs
+++ TestSFTP/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Renci.SshNet;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,8 +15,8 @@ namespace TestSFTP
             string host = args[0];
             string username = args[1];
             string password = args[2];
-
             string remoteDirectory = args[3];
+            
 
             using (SftpClient sftp = new SftpClient(host, username, password))
             {
@@ -24,10 +25,20 @@ namespace TestSFTP
                     sftp.Connect();
 
                     var files = sftp.ListDirectory(remoteDirectory);
-
+                    sftp.CreateDirectory(remoteDirectory + "/test");
                     foreach (var file in files)
                     {
                         Console.WriteLine(file.Name);
+                        var newFile = Path.Combine("test", file.Name);
+                        Directory.CreateDirectory(Path.GetDirectoryName(newFile));
+                        using (FileStream stream = new FileStream(newFile, FileMode.Create, FileAccess.ReadWrite, FileShare.None))
+                        {
+                            sftp.DownloadFile(file.FullName, stream);
+                        }
+                        using (FileStream stream = new FileStream(newFile, FileMode.Open, FileAccess.ReadWrite, FileShare.None))
+                        {
+                            sftp.UploadFile(stream, string.Format("{0}/{1}/{2}", remoteDirectory, "test", file.Name),true);
+                        }
                     }
                     sftp.Disconnect();
                 }
