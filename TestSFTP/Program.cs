eb7ef7a6591ff8998a529cba759f37a293300b41
Fermin Ricolfe Loren <f.ricolfe@olrsoft.com>
2020-07-17 15:35:22 +0200

    Agregar archivos de proyecto.

---
+++ TestSFTP/Program.cs
@@ -0,0 +1,41 @@
+﻿using Renci.SshNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestSFTP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string host = args[0];
+            string username = args[1];
+            string password = args[2];
+
+            string remoteDirectory = args[3];
+
+            using (SftpClient sftp = new SftpClient(host, username, password))
+            {
+                try
+                {
+                    sftp.Connect();
+
+                    var files = sftp.ListDirectory(remoteDirectory);
+
+                    foreach (var file in files)
+                    {
+                        Console.WriteLine(file.Name);
+                    }
+                    sftp.Disconnect();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("An exception has been caught " + e.ToString());
+                }
+            }
+        }
+    }
+}
